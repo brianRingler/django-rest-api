69b32b80258984e02294ec47abcfde762b47e13e
Brian Ringler <ringler.brian.8286@gmail.com>
2021-04-29 09:13:51 -0400

    views.py PUT PATCH and DELETE addded to class

--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -31,5 +31,47 @@ class HelloApiView(APIView):
         variable we created above. This value should have max char of 10.'''
         serializer = self.serializer_class(data=request.data)
 
+        # There are multiple validators we can use to validate 
         if serializer.is_valid():
-            name = serializer.validation_data.get('name')
+            # Here we are validating that the name is at least 10 characters
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            # if we have status 200 return a dict response object of our validated message
+            return Response({'message' : message})
+        else:
+            # if not status 200 inform requester of bad request
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                 )
+
+    def put(self, request, pk=None):
+        '''
+        Handle updating an entire object based on primary key. This would 
+        be same as ModelName.objects.get(id=pk)'''
+        # Here we are just returning a response and not updating our empty db
+        return Response({
+                        'id' : pk,
+                        'method' : 'PUT'
+                        })
+
+    def patch(self, request, pk=None):
+        '''patch will handle a partial update of an object. If we had a model with
+        first_name, last_name, dob, favorite_food then this would update on the first
+        and name for the user. We would query the db and then only update required
+        fields based on keys'''
+        return Response({
+                        'id' : pk,
+                        'first_name' : 'Teddy' , 
+                        'last_name' : 'Cool', 
+                        'method' : 'PATCH'
+                        })
+    def delete(self, request, pk=None):
+        '''Like PUT and UPDATE we are going to DELETE a record from our 
+        db table.'''
+        return Response({
+                        'id' : pk,
+                        'first_name' : 'Teddy', 
+                        'last_name' : 'Cool', 
+                        'method' : 'DELETE'
+                        })
